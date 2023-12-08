0fcc7dfe0f884c16de6f0cb8c00c364cd1492bf2
Shivansh Rawat <shivanshrawat587@gmail.com>
2023-12-08 12:07:09 +0530

    added scratch login

--- src/App.js
+++ src/App.js
@@ -19,56 +19,66 @@ function App() {
 
   const { q } = useParams();
 
-  function reverseFirstFiveChars(inputString) {
-    if (inputString.length < 5) {
-      // If the string has less than 5 characters, return it as it is
-      return inputString;
-    }
-
-    // Split the string into an array of characters
-    const charArray = inputString.split("");
-
-    // Reverse the first five characters
-    const reversedChars = charArray.slice(0, 5).reverse();
-
-    // Join the reversed characters with the rest of the string
-    return reversedChars.concat(charArray.slice(5)).join("");
+  function reverseJwtBody(jwt) {
+    const [header, body, signature] = jwt.split(".");
+    const reversedBody = body.split("").reverse().join("");
+    return [header, reversedBody, signature].join(".");
   }
 
   const handleCallbackResponse = async (response) => {
-    console.log("encoded data JWT: " + response.credential);
     setLoading(true);
     let jwtToken = response.credential;
+    localStorage.setItem("token", jwtToken);
+
+    axios({
+      url: `https://merd-api.merakilearn.org/users/auth/google`,
+      method: "post",
+      headers: { accept: "application/json", Authorization: jwtToken },
+      data: { idToken: jwtToken, mode: "web" },
+    }).then((res) => {
+      console.log("res", res.data.token);
+      axios
+        .get("https://merd-api.merakilearn.org/users/addSessionToken")
+        .then((response) => {
+          console.log(response.data, "response data");
+
+          const reversedString = reverseJwtBody(jwtToken);
+          console.log(document.referrer, "document referrer");
+          if (document.referrer == "http://localhost:8080/"|| document.referrer == "https://dev.scratch.merakilearn.org/") {
+            window.location.href =
+              document.referrer +
+              "login/?token=" +
+              res.data.token +
+              "&loggoutToken=" +
+              response.data;
+            console.log(document.referrer, "document referrer");
+          }else{
+            window.location.href =
+            document.referrer +
+            "?token=" +
+            res.data.token +
+            "&loggoutToken=" +
+            response.data;
+          console.log(document.referrer, "document referrer");
+          }
+        });
+    });
 
     // Decode the JWT token to get the user ID
-    const decodedToken = jwt_decode(response.credential);
-    console.log(decodedToken);
-    localStorage.setItem("token", response.credential);
+    // const decodedToken = jwt_decode(response.credential);
+    // console.log(decodedToken);
+    // localStorage.setItem("token", response.credential);
 
-    const reversedString = reverseFirstFiveChars(jwtToken);
-
-    let generatedSessionToken = await axios.get(
-      "http://localhost:5001/users/addSessionToken"
-    );
-    console.log("GeneratedToken:", generatedSessionToken.data);
-    localStorage.setItem("loggoutToken", generatedSessionToken.data);
-
-    console.log("Modified String:", reversedString);
-
-    window.location.href =
-      document.referrer +
-      "?token=" +
-      reversedString +
-      "&loggoutToken=" +
-      generatedSessionToken.data;
-    console.log(document.referrer, "document referrer");
+    // let generatedSessionToken = await
+    // console.log("GeneratedToken:", generatedSessionToken.data);
+    // localStorage.setItem("loggoutToken", generatedSessionToken.data);
   };
 
   let qValue;
   useEffect(() => {
     async function Logout() {
       let removedSessionToken = await axios.get(
-        `http://localhost:5001/users/removeSessionToken?token=${localStorage.getItem(
+        `https://merd-api.merakilearn.org/users/removeSessionToken?token=${localStorage.getItem(
           "loggoutToken"
         )}`
       );
@@ -88,26 +98,31 @@ function App() {
     console.log("first useeffect is running", loggedOutState);
     let storedToken = localStorage.getItem("token");
 
-    if (loggedOutState == "true") {
-      Logout();
-      window.location.href = document.referrer;
-    } else if (document.referrer && storedToken) {
-      // TODO: SECURITY ISSUE: document.referrer needs to be checked against whitelist of URLS, DON'T LEAK TOKEN
-      window.location.href =
-        document.referrer +
-        "?token=" +
-        reverseFirstFiveChars(storedToken) +
-        "&loggoutToken=" +
-        localStorage.getItem("loggoutToken");
-    } /*if (loggedOutState == "false") {
+    // if (loggedOutState == "true") {
+    //   Logout();
+    //   window.location.href = document.referrer;
+    // } else if (document.referrer && storedToken) {
+    //   // TODO: SECURITY ISSUE: document.referrer needs to be checked against whitelist of URLS, DON'T LEAK TOKEN
+    //   window.location.href =
+    //     document.referrer +
+    //     "?token=" +
+    //     reverseJwtBody(storedToken) +
+    //     "&loggoutToken=" +
+    //     localStorage.getItem("loggoutToken");
+    // }
+    if (
+      loggedOutState == "false" &&
+      localStorage.getItem("token") &&
+      localStorage.getItem("loggoutToken")
+    ) {
       console.log("second useeffect is running");
-      window.location.href =
-        document.referrer + "?token=" + reverseFirstFiveChars(storedToken);
-    } else if (isFirstLogin == "true") {
-      console.log("third useeffect is running");
-      window.location.href =
-        document.referrer + "?token=" + reverseFirstFiveChars(storedToken);
-    }*/
+      // window.location.href =
+      //   document.referrer +
+      //   "?token=" +
+      //   reverseJwtBody(
+      //     storedToken + "&loggoutToken=" + localStorage.getItem("loggoutToken")
+      //   );
+    }
 
     // Log the 'q' parameter value
     console.log("Value of 'q' parameter:", qValue);
@@ -122,8 +137,6 @@ function App() {
       width: 200,
       size: "large",
     });
-
-    document.title = "Accounts Navgurukul";
   }, []);
 
   return (
@@ -138,6 +151,7 @@ function App() {
             <div id="signInDiv" className="custom-google-button">
               Login with Google
             </div>
+
             {loading ? (
               <img src={loader} alt="loader" id="loading-image" />
             ) : null}
